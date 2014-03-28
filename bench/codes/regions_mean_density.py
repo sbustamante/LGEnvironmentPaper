56417dd80b444181c0224f2c4859b68a2e477928
sbustamante <macsebas33@gmail.com>
2014-03-28 21:28:23 +0000

    Integrating the plot of mean density of different regions into a single figure in the sake of saving

--- bench/codes/regions_mean_density.py
+++ bench/codes/regions_mean_density.py
@@ -23,6 +23,8 @@ smooth = '_s1'
 catalog = 'FOF'
 #Web Scheme
 webs = ['Vweb', 'Tweb']
+#Styles
+color = ['blue','green']
 
 #N Lambda
 N_l = 100
@@ -37,7 +39,8 @@ L_max = 2
 
 N_sim = len(folds)
 
-plt.figure( figsize=(6.0,2*4) )
+plt.figure( figsize=(6.0,4) )
+i_web = 0
 for web in webs:
     i_fold = 0
     for fold in folds:
@@ -62,42 +65,25 @@ for web in webs:
 	#Plots the results
 	
 	#Voids
-	plt.subplot( 2,1,1 )
-	plt.plot( lambda_th, regs[1]/(1+regs[5]), linewidth = 2, label = '%s'%(web) )
-	#Tweb threshold
-	plt.vlines( 0.3262, -1.0, 1.0, linestyle = '--', color='green', linewidth = 2 )
-	#Vweb threshold
-	plt.vlines( 0.1884, -1.0, 1.0, linestyle = '--', color='blue', linewidth = 2 )
-	
+	plt.plot( lambda_th, regs[1]/(1+regs[5]), color = color[i_web], linestyle = '--', linewidth = 2, label = '%s Voids'%(web) )
+
 	#Sheets
-	plt.subplot( 2,1,2 )
-	plt.plot( lambda_th, regs[2]/(1+regs[6]), linewidth = 2, label = '%s'%(web) )
-	#Tweb threshold
-	plt.vlines( 0.3262, -1.0, 1.0, linestyle = '--', color='green', linewidth = 2 )
-	#Vweb threshold
-	plt.vlines( 0.1884, -1.0, 1.0, linestyle = '--', color='blue', linewidth = 2 )
-	plt.hlines( 0.0, 0.0, 1.0, linestyle = '--', color='gray', linewidth = 2 )
+	plt.plot( lambda_th, regs[2]/(1+regs[6]), color = color[i_web], linestyle = '-', linewidth = 2, label = '%s Sheets'%(web) )
 	
 	i_fold += 1
-    
+    i_web += 1
 
 #Mean Density
-plt.subplot( 2,1,1 )
+#Tweb threshold
+plt.vlines( 0.3262, -1.0, 1.0, linestyle = '--', color='green', linewidth = 2 )
+#Vweb threshold
+plt.vlines( 0.1884, -1.0, 1.0, linestyle = '--', color='blue', linewidth = 2 )
+plt.hlines( 0.0, 0.0, 1.0, linestyle = '--', color='gray', linewidth = 2 )
 plt.ylabel( "$\\bar \delta$" )
 plt.xlabel( "$\lambda_{th}$" )
 plt.grid()
 plt.ylim( (-1.0, 1.0) )
 plt.xlim( (0.0, 1.0) )
-plt.text( 0.0, 0.5, ' Voids' )
-plt.legend( loc='upper right', fancybox = True, shadow = True, ncol = 1)
-
-plt.subplot( 2,1,2 )
-plt.ylabel( "$\\bar \delta$" )
-plt.xlabel( "$\lambda_{th}$" )
-plt.grid()
-plt.ylim( (-1.0, 1.0) )
-plt.xlim( (0.0, 1.0) )
-plt.text( 0.0, 0.5, ' Sheets' )
-
+plt.legend( loc='upper right', fancybox = True, shadow = True, ncol = 1, prop={'size':10} )
 
 plt.show()
